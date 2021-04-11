12b3cee591bbc492793e885969ff135f6508d5d3
yoooel <72613221+yoooel@users.noreply.github.com>
2021-04-11 14:48:42 +0000

    other divs added to body

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
-// testing js 
-// alert("i am working fine");
-// Adding some elements to the div with id of wrapper
-const para = document.createElement("div");
-const node = document.createTextNode("This is new.");
-para.appendChild(node);
 
-const element = document.getElementById("wrapper");
-element.appendChild(para);
+// Adding some elements to the div with id of main-container
+let div = document.querySelector('#main-container'); 
+div.classList.add("left-container"); 
+
+div.insertAdjacentElement( 'afterbegin', 
+`
+<div>
+<img id="pic1" src="assets/frontview.jpg" alt="">
+</div>
+<div>
+<img id="pic2" src="assets/kiddo.jpg" alt="">
+</div>
+<div>
+<img id="pic3" src="assets/backview.jpg" alt="">
+</div>
+
+`)
+// Adding right hand side div 
+let div2 = document.querySelector('.left-container'); 
+div2.classList.add('right-container'); 
+div2.insertAdjacentElement('afterend', `
+
+<div>
+<img id="img1" src="assets/frontview.jpg" alt="">
+</div>
+<div>
+<img id="img2" src="assets/kiddo.jpg" alt="">
+</div>
+<div>
+<img id="img3" src="assets/backview.jpg" alt="">
+</div>
+
+`)
